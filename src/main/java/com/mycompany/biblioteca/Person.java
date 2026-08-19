ceffe290aea65a6a574d030cf137d313cf6f4baa
Geovanny Pino <geosant1911@gmail.com>
2026-08-18 20:46:52 -0500

    feat: crear clase Person como padre de Cliente.

---
+++ src/main/java/com/mycompany/biblioteca/Person.java
@@ -0,0 +1,10 @@
+package com.mycompany.biblioteca;
+
+public class Person {
+
+    private String name;
+    private String identification;
+    private String telephone;
+    private String mail;
+
+}
